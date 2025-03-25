c14d35ea5fc6e59a23a6a9fb65a7ad306c5a38ba
Jesua <180544338+jesua-cell@users.noreply.github.com>
2025-03-25 11:05:30 -0400

    manejo de errores en los controladores de las rutas (Error Handling)

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -2,37 +2,64 @@ import { pool } from '../db.js'
 
 
 export const getTasks = async (req, res) => {
-    const [result] = await pool.query("SELECT * FROM tasks ORDER BY createAt ASC"); //Obtener todas los datos(tareas; objetos)
-    res.json(result)
-    console.log(result);
+
+    try {
+        const [result] = await pool.query("SELECT * FROM tasks ORDER BY createAt ASC"); //Obtener todas los datos(tareas; objetos)
+        res.json(result)
+        console.log(result);
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
 };
 
 export const getTask = async (req, res) => {
-    const [result] = await pool.query('SELECT * FROM tasks WHERE id = ?', [req.params.id]); //Obtener un dato(tarea; objeto) en especifico indicado por los parametros de la URL('/:id') 
-    console.log(req.params.id);
-    if(result.length === 0) {return res.status(404).json({message: "Tarea no encontrada"})} //Validacion y verificacion para obtener un dato que exista en la BD
-    res.send(result[0])
+
+    try {
+        const [result] = await pool.query('SELECT * FROM tasks WHERE id = ?', [req.params.id]); //Obtener un dato(tarea; objeto) en especifico indicado por los parametros de la URL('/:id') 
+        console.log(req.params.id);
+        if (result.length === 0) { return res.status(404).json({ message: "Tarea no encontrada" }) } //Validacion y verificacion para obtener un dato que exista en la BD
+        res.send(result[0])
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
 };
 
 export const createTasks = async (req, res) => {
-    const { title, description } = req.body //Obtener los datos del client
-    const [result] = await pool.query("INSERT INTO tasks(title, description) VALUES (?, ?)", [title, description]) //Crear nuevos datos(tareas; objetos)
-    console.log(result);
-    res.send({
-        id: result.insertId,
-        title,
-        description
-    })
+
+    try {
+        const { title, description } = req.body //Obtener los datos del client
+        const [result] = await pool.query("INSERT INTO tasks(title, description) VALUES (?, ?)", [title, description]) //Crear nuevos datos(tareas; objetos)
+        console.log(result);
+        res.send({
+            id: result.insertId,
+            title,
+            description
+        })
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+
 };
 
 export const updateTasks = async (req, res) => {
-     const [result] = await pool.query("UPDATE tasks SET ? WHERE id = ?", [req.body, req.params.id]); // 1: Obtener el cuerpo del objeto(tareas); 2: Obtener el id del objeto(tarea). Primera consulta: (SET ? -> req.body), y segunda consulta: (WHERE id = ? -> req.params.id) 
-     res.json(result)
+
+    try {        
+        const [result] = await pool.query("UPDATE tasks SET ? WHERE id = ?", [req.body, req.params.id]); // 1: Obtener el cuerpo del objeto(tareas); 2: Obtener el id del objeto(tarea). Primera consulta: (SET ? -> req.body), y segunda consulta: (WHERE id = ? -> req.params.id) 
+        res.json(result)
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
 };
 
 export const deleteTasks = async (req, res) => {
-    const [result] = await pool.query("DELETE FROM tasks WHERE id = ?", [req.params.id]); //Eliminar un dato(tarea; objeto) en espesifico por medio de la URL('/:id')
-    if(result.affectedRows === 0) {return res.status(404).json({message: "Tarea a eliminar no encontrada"})} // Validacion y verificacion para obtener un dato que exista en la BD
-    return res.sendStatus(204)
+
+    try {
+        const [result] = await pool.query("DELETE FROM tasks WHERE id = ?", [req.params.id]); //Eliminar un dato(tarea; objeto) en espesifico por medio de la URL('/:id')
+        if (result.affectedRows === 0) { return res.status(404).json({ message: "Tarea a eliminar no encontrada" }) } // Validacion y verificacion para obtener un dato que exista en la BD
+        return res.sendStatus(204)
+    } catch (error) {
+        return res.status(500).json({message: error.message})        
+    }
+
 };
 
