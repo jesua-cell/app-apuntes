bcea10f91ad8a9d0047c5e51d46a1c0a535670eb
Jesua <180544338+jesua-cell@users.noreply.github.com>
2025-03-26 14:32:42 -0400

    enlace de la URL del Front con el Back y añadiendo metodos al formulario

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ import express from 'express'
 import { PORT } from './config.js';
 import indexRoutes from './routes/index.routes.js'
 import taskRoutes from './routes/tasks.routes.js'
+import cors from 'cors'
 
 const app = express()
+
+//Manejo del puerto del Front al Back:
+app.use(cors({origin: 'http://localhost:5173'}))
 
 //Procesar los datos de cliente:
 app.use(express.json())
@@ -13,5 +17,3 @@ app.use(taskRoutes)
 
 app.listen(PORT)
 console.log(`Servidor ejecutandose en: http://localhost:${PORT} `);
-
-//https://youtu.be/dJbd7BYofp4?t=2289
