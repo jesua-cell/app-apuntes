6718927a023d13eb68a9328534d281ab36a1843b
Jesua <180544338+jesua-cell@users.noreply.github.com>
2025-03-26 12:23:50 -0400

    manejo de eventos del formulario usando Formik

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+/**
+ * Formik: Mantiene el estado
+ * Form: Crea el formulario
+ */
+import { Form, Formik } from 'formik'
 
 export const TaskForm = () => {
     return (
         <>
-            <div>TaskForm</div>
+            <div>
+                <Formik
+                    initialValues={{ //Valor iniciales
+                        title: "", //valor obtenudo de: name="title"
+                        description: "" //valor obtenudo de: name="description"
+                    }}
+                    onSubmit={(values) => { //Metodo: Enviar datos desde el Formulario
+                        console.log(values);
+                    }}
+                >
+                    {({ handleChange, handleSubmit }) => ( //Funciones del Formulario: Mannejo de Errores y Manejo de Envio
+                        <Form onSubmit={handleSubmit}>
+                            <label>Titulo</label>
+                            <input
+                                type="text"
+                                name='title'
+                                placeholder='Escriba un Titulo'
+                                onChange={handleChange} />
+
+                            <label>Descripcion</label>
+                            <textarea
+                                name="description"
+                                rows="3"
+                                placeholder='Escriba una descripcion'
+                                onChange={handleChange}
+                            ></textarea>
+
+                            <button type='submit'>Guardar</button> 
+                            {/* el type='submit'activa el onSubmit */}
+                        </Form>
+                    )}
+                </Formik>
+            </div>
         </>
     )
 }
