2429fa59d88cad88868bc2df98e216eceeaa4bb8
Jesua <180544338+jesua-cell@users.noreply.github.com>
2025-03-28 10:28:19 -0400

    compilacion, contruccion y paquetacion del proyecto (Build)

--- server/index.js
+++ server/index.js
@@ -3,17 +3,27 @@ import { PORT } from './config.js';
 import indexRoutes from './routes/index.routes.js'
 import taskRoutes from './routes/tasks.routes.js'
 import cors from 'cors'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path';
+import { json } from 'stream/consumers';
 
 const app = express()
 
 //Manejo del puerto del Front al Back:
-app.use(cors({origin: 'http://localhost:5173'}))
+app.use(cors({ origin: 'http://localhost:5173' }))
+
+//rutas de las carpetas del proyecto:
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
 
 //Procesar los datos de cliente:
 app.use(express.json())
 
 app.use(indexRoutes)
 app.use(taskRoutes)
+
+//Archivos estaticos:
+app.use(express.static(join(__dirname, '../client/dist')));
 
 app.listen(PORT)
 console.log(`Servidor ejecutandose en: http://localhost:${PORT} `);
